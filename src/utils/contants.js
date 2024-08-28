4bab6272740d0f3964b5f2d4923a9cb8f8ef3140
kartikgarg9 <KARTIKGARG20@GMAIL.COM>
2024-08-28 16:38:14 +0530

    cors

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
   "https://cdn.dribbble.com/users/1635051/screenshots/4291569/socio_curry_logo-01.png";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6528057&lng=77.09655579999999&restaurantId=";
+  "https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D28.6528057%26lng%3D77.09655579999999%26restaurantId%3D";
